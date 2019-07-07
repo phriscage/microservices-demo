100b6cb615e8cf4f03963af75aaec5b0f690c14b
phriscage <phriscage@gmail.com>
2019-07-06 21:05:55 -0400

    added cookie id-token and updated user profile menu

--- src/frontend/static/js/login.js
+++ src/frontend/static/js/login.js
@@ -26,22 +26,25 @@ var handleSignedInUser = function(user) {
 			document.getElementById('user-signed-in').className = 'd-block';
 			document.getElementById('name').value = 'Hi ' + user.displayName + '!';
 			document.getElementById('email').value = user.email;
-			user.getIdToken().then(function(idToken) {
-				var jwtIoUrl = "'https://jwt.io/?value=" + idToken + "'";
+			user.getIdTokenResult().then(function(idTokenResult) {
+				var jwtIoUrl = "'https://jwt.io/?value=" + idTokenResult.token + "'";
 				var idTokenLabel = 'ID Token: | <a target="_blank" href=' + jwtIoUrl + '>jwt.io</a>';
-				document.getElementById('id-token').value = idToken;
+				// let's set the idToken as a cookie for demo
+				setCookie('firebase_id-token', idTokenResult.token, idTokenResult.expirationTime);
+				document.getElementById('id-token').value = idTokenResult.token;
 				document.getElementById('id-token-label').innerHTML = idTokenLabel;
 			});
 		};
 		if (document.getElementById('user-signed-out') != null) {
 			document.getElementById('user-signed-out').className = 'd-none';
 		};
-		if (document.getElementById('user-signed-in-dropdown') != null) {
-			document.getElementById('user-signed-in-dropdown').className = 'd-block';
-			document.getElementById('dropdown-email').innerHTML = user.email;
+		if (document.getElementById('user-signed-in-menu') != null) {
+			document.getElementById('user-signed-in-menu').className = 'd-block';
+			// document.getElementById('user-profile-menu').innerHTML = '<a href="/login">' + user.email + '</a>';
+			document.getElementById('user-profile-menu').innerHTML = user.email;
 		};
-		if (document.getElementById('user-signed-out-dropdown') != null) {
-			document.getElementById('user-signed-out-dropdown').className = 'd-none';
+		if (document.getElementById('user-signed-out-menu') != null) {
+			document.getElementById('user-signed-out-menu').className = 'd-none';
 		};
 };
 
@@ -49,18 +52,18 @@ var handleSignedInUser = function(user) {
  * Displays the UI for a signed out user.
  */
 var handleSignedOutUser = function() {
-		// if (document.getElementById('user-signed-in') != null) {
+		eraseCookie('firebase_id-token');
 		if (document.getElementById('user-signed-in')) {
 			document.getElementById('user-signed-in').className = 'd-none';
 		};
 		if (document.getElementById('user-signed-out') != null) {
 			document.getElementById('user-signed-out').className = 'd-block';
 		};
-		if (document.getElementById('user-signed-in-dropdown') != null) {
-			document.getElementById('user-signed-in-dropdown').className = 'd-none';
+		if (document.getElementById('user-signed-in-menu') != null) {
+			document.getElementById('user-signed-in-menu').className = 'd-none';
 		};
-		if (document.getElementById('user-signed-out-dropdown') != null) {
-			document.getElementById('user-signed-out-dropdown').className = 'd-block';
+		if (document.getElementById('user-signed-out-menu') != null) {
+			document.getElementById('user-signed-out-menu').className = 'd-block';
 		};
 };
 
@@ -70,6 +73,13 @@ var handleSignedOutUser = function() {
  document.getElementById('sign-out').addEventListener('click', function() {
 		console.log('signOut');
 		firebase.auth().signOut();
+		window.location.replace('/login');
+});
+
+/**
+ * handle the user profile menu button click
+ */
+ document.getElementById('user-profile-menu').addEventListener('click', function() {
 		window.location.replace('/login');
 });
 
@@ -104,3 +114,39 @@ document.addEventListener('DOMContentLoaded', function() {
 				};
 		};
 });
+
+/**
+ * Utils
+ */
+
+/**
+ * Set Cookie
+ * @param {name, value, days}
+ */
+function setCookie(name,value,expiresTime) {
+    var expires = "; expires=" + expiresTime;
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+/**
+ * Get Cookie
+ * @param {name}
+ */
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+/**
+ * erase Cookie
+ * @param {name}
+ */
+function eraseCookie(name) {
+    document.cookie = name+'=; Max-Age=-99999999;';
+}
